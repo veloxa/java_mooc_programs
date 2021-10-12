7344a141c6a787bf687c89cfab26e8259d3f9aed
veloxa <110683787+veloxa@users.noreply.github.com>
2021-10-12 11:45:06 +0000

    Regenerate gemspec for version 1.0.2

--- spec/coin_combo_integration_spec.rb
+++ spec/coin_combo_integration_spec.rb
@@ -5,4 +5,4 @@ Capybara.app = Sinatra::Application
 describe('the result path', {:type => :feature}) do
   it('processes coin amounts and returns change') do
     visit('/')
-    fill_in('input', :w
+    fill_in('input', :wi
