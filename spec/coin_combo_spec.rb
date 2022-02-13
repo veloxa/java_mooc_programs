fb81a6bcde6e8619d4af00490a20661dfc0be117
veloxa <110683787+veloxa@users.noreply.github.com>
2022-02-13 06:20:58 +0000

    bumped version for release

--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -5,3 +5,4 @@ require('coin_combo')
 describe('Fixnum#coin_combo') do
 it('takes an amount of cents and shows the smallest number of quarters needed to make change') do
   expect(125.coin_combo()).to(include('5 quarters'))
+end
