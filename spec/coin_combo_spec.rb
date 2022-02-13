ccddf2a066b5a7d6445eb5a3a9aec8cdf99dd7d7
veloxa <110683787+veloxa@users.noreply.github.com>
2022-02-13 07:23:44 +0000

    readme tweak

--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -6,3 +6,6 @@ describe('Fixnum#coin_combo') do
 it('takes an amount of cents and shows the smallest number of quarters needed to make change') do
   expect(125.coin_combo()).to(include('5 quarters'))
 end
+
+it('takes an amount of cents and shows the smallest number of dimes needed to make change') do
+  expect(20.coin_combo()).to(include('2 dimes'))
