574930f49bd6a7f0adae9440b25f5ec038e9de7d
veloxa <110683787+veloxa@users.noreply.github.com>
2022-02-19 16:53:23 +0000

    stopping point

--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -14,3 +14,4 @@ end
 it('taskes an amount of cents and show the smallest numbe rof nickels needed to make change') do
   expect(5.coin_combo()).to(include('1 nickel'))
 end
+it ('takes an amount of cents and shows the smallest number of pennies needed to make change') do
